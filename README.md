010cb0d7a051266486b0e961320d5afd55e93b6b
nouriach <nouriach17@gmail.com>
2025-10-07 20:20:50 +0100

    Initial Project setup: DataManager & Core

---
+++ README.md
@@ -0,0 +1,7 @@
+# Notes
+
+## RecipePlatform.DataManager
+- Eventually we will need to create a Function URL to make the endpoints accessible
+
+## RecipePlatform.Core
+- This will hold any shared models
---
+++ src/RecipePlatform.DataManager/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/hello", () => "hello");
+
+app.Run();
